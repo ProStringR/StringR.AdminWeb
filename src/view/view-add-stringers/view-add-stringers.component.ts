75477a266cbef16800e6f8078c553732e45b6003
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-09-24 20:45:27 +0200

    Add list of stringers and potential stringers

--- src/view/view-add-stringers/view-add-stringers.component.ts
+++ src/view/view-add-stringers/view-add-stringers.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAddStringersComponent implements OnInit {
 
+  private listOfYourStringers: Array<string> = new Array<string>();
+  private listOfPotentialStringers: Array<string> = new Array<string>();
+
   constructor() { }
 
   ngOnInit() {
+    this.listOfYourStringers.push('Jaafar', 'Nadia', 'Steen', 'Liv');
+    this.listOfPotentialStringers.push('Marcus', 'Frederikke', 'Frederik', 'Milisha')
   }
 
+  getListOfYourStringers(): Array<string> {
+    return this.listOfYourStringers;
+  }
+
+  getListOfPotentialStringers(): Array<string> {
+    return this.listOfPotentialStringers;
+  }
+
+  onPotentialStringerClicked(index: number) {
+    this.listOfYourStringers.push(this.listOfPotentialStringers[index]);
+
+    const stringerToRemove = this.listOfPotentialStringers[index];
+    this.listOfPotentialStringers = this.listOfPotentialStringers.filter(obj => obj !== stringerToRemove);
+
+  }
+
+  onYourStringerClicked(index: number) {
+    this.listOfPotentialStringers.push(this.listOfYourStringers[index]);
+
+    const stringerToRemove = this.listOfYourStringers[index];
+    this.listOfYourStringers = this.listOfYourStringers.filter(obj => obj !== stringerToRemove);
+
+  }
 }
