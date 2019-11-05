48bcdd92b3c545389c5525c6ede62f7ae5f6723a
Marcus August Christiansen <s175185@student.dtu.dk>
2019-11-05 10:45:16 +0100

    get data from the storage component

--- src/app/list-cell/storage-cell/storage-cell.component.ts
+++ src/app/list-cell/storage-cell/storage-cell.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { DataControlService } from 'src/app/control/data-control.service';
+import {element} from 'protractor';
 
 @Component({
   selector: 'app-storage-cell',
@@ -7,6 +8,8 @@ import { DataControlService } from 'src/app/control/data-control.service';
   styleUrls: ['./storage-cell.component.scss']
 })
 export class StorageCellComponent implements OnInit {
+
+  @Input() elementName: string;
 
   constructor(private dataControl: DataControlService) { }
 
--- src/app/view/view-storage-management/view-storage-management.component.ts
+++ src/app/view/view-storage-management/view-storage-management.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { DataControlService } from '../../control/data-control.service';
-import { OrderModel } from 'src/app/model/model-order';
-import { RacketStringModel } from 'src/app/model/model-racket-string';
 
 @Component({
   selector: 'app-view-storage-management',
@@ -10,14 +8,10 @@ import { RacketStringModel } from 'src/app/model/model-racket-string';
 })
 export class ViewStorageManagementComponent implements OnInit {
 
-  storageList = ["Hello", "You"];
+  storageList = ['Hello', 'You'];
 
   constructor(private dataControl: DataControlService) { }
 
-  ngOnInit() {
-    this.dataControl.getList<RacketStringModel>('https://prostringr.firebaseio.com/storage/ShopMJ.json').subscribe(res => {
-      console.log(res)
-    })
-  }
+  ngOnInit() {}
 
 }
