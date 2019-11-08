a7eb3e7c6b09623abc4311a9e3c1bb93a94c594e
Marcus August Christiansen <s175185@student.dtu.dk>
2019-11-08 21:52:39 +0100

    purchase history cell made to a date string

--- src/app/list-cell/purchase-history-cell/purchase-history-cell.component.ts
+++ src/app/list-cell/purchase-history-cell/purchase-history-cell.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {PurchaseHistory} from '../../model/model-racket-string';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-purchase-history-cell',
@@ -9,10 +10,13 @@ import {PurchaseHistory} from '../../model/model-racket-string';
 export class PurchaseHistoryCellComponent implements OnInit {
 
   @Input() history: PurchaseHistory;
+  purchaseDate: string;
 
-  constructor() { }
+  constructor(public datePipe: DatePipe) { }
 
   ngOnInit() {
+    const date = new Date(this.history.date);
+    this.purchaseDate = this.datePipe.transform(date, 'dd/MM/yyyy');
   }
 
 }
