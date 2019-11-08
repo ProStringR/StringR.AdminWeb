9af9624756c14da6588de0ec8b14b3de9eaa4e22
Marcus August Christiansen <s175185@student.dtu.dk>
2019-11-08 21:51:22 +0100

    string detail create - use in collapse body

---
+++ src/app/view/view-string-detail/view-string-detail.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {RacketStringService} from '../../service/racket-string.service';
+import {RacketStringModel} from '../../model/model-racket-string';
+
+@Component({
+  selector: 'app-view-string-detail',
+  templateUrl: './view-string-detail.component.html',
+  styleUrls: ['./view-string-detail.component.scss']
+})
+export class ViewStringDetailComponent implements OnInit {
+
+  @Input() element: RacketStringModel;
+  topRowLabels = ['Brand', 'Model', 'Type'];
+  bottomRowLabels = ['Remaining Rackets', 'Price Per Racket'];
+  topRowValues = [];
+  bottomRowValues = [];
+
+  constructor(private racketStringService: RacketStringService) { }
+
+  ngOnInit() {
+    this.topRowValues = [this.element.brand, this.element.modelName, this.element.stringType];
+    this.bottomRowValues = [this.racketStringService.getRacketsRemaining(this.element), this.element.pricePerRacket];
+  }
+
+}
