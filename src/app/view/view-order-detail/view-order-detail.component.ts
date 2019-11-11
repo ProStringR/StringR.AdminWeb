8ef722b07d408a884bd7993905be74861d4e1f24
Marcus August Christiansen <s175185@student.dtu.dk>
2019-11-11 20:37:20 +0100

    order detail to set to the list tap body

--- src/app/view/view-order-detail/view-order-detail.component.ts
+++ src/app/view/view-order-detail/view-order-detail.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {OrderModel} from '../../model/model-order';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-view-order-detail',
@@ -10,9 +11,28 @@ export class ViewOrderDetailComponent implements OnInit {
 
   @Input() element: OrderModel;
 
-  constructor() { }
+  firstLabelRow = ['String Brand', 'String Model', 'String Type'];
+  firstValueRow = [];
+  secondLabelRow = ['Tension', 'Price', 'Delivery Date'];
+  secondValueRow = [];
+  thirdLabelRow = ['Racket Brand', 'Racket Model', 'Stringer'];
+  thirdValueRow = [];
+  fourthLabelRow = ['Customer', 'Phone Number', 'Email'];
+  fourthValueRow = [];
+
+  labels = [this.firstLabelRow, this.secondLabelRow, this.thirdLabelRow, this.fourthLabelRow];
+  values = [this.firstValueRow, this.secondValueRow, this.thirdValueRow, this.fourthValueRow];
+
+  constructor(public datePipe: DatePipe) { }
 
   ngOnInit() {
+    this.firstValueRow = [this.element.stringId, this.element.stringId, this.element.stringId];
+    this.secondValueRow =
+      ['M: ' + this.element.tensionVertical + ' C: ' + this.element.tensionVertical,
+      this.element.price,
+      this.datePipe.transform(new Date(this.element.deliveryDate), 'dd/MM/yyyy')];
+    this.thirdValueRow = [this.element.racketId, this.element.racketId, this.element.racketId];
+    this.fourthValueRow = [this.element.customerId, this.element.customerId, this.element.customerId];
   }
 
 }
