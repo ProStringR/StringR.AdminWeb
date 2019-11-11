6119272b1f9857d081a893f45eaf7a6547a49ca2
Marcus August Christiansen <s175185@student.dtu.dk>
2019-11-11 20:36:49 +0100

    add semicolon

--- src/app/model/model-order.ts
+++ src/app/model/model-order.ts
@@ -1,24 +1,24 @@
 export interface OrderModel {
-    comment: string
-    customerId: string
-    deliveryDate: number
-    orderHistory: OrderHistory[]
-    orderId: string
-    orderStatus: string
-    paid: boolean
-    price: number
-    racketId: string
-    racketType: string
-    shopId: string
-    stringId: string
-    stringerId: string
-    tensionHorizontal: number
-    tensionVertical: number
-    timePlaced: number
+    comment: string;
+    customerId: string;
+    deliveryDate: number;
+    orderHistory: OrderHistory[];
+    orderId: string;
+    orderStatus: string;
+    paid: boolean;
+    price: number;
+    racketId: string;
+    racketType: string;
+    shopId: string;
+    stringId: string;
+    stringerId: string;
+    tensionHorizontal: number;
+    tensionVertical: number;
+    timePlaced: number;
 }
 
 export interface OrderHistory {
-    date: number
-    orderStatus: string
-    paid: boolean
+    date: number;
+    orderStatus: string;
+    paid: boolean;
 }
