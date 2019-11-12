ff0d330a542c64b1b38cdca0c13d62a6f77aa159
Steen Hansen <steen_home@hotmail.com>
2019-11-12 12:51:55 +0100

    Lavet første store service som indeholder racketStrings

---
+++ src/app/store/storage-management.store.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { observable, action } from 'mobx-angular';
+import { RacketStringModel } from '../model/model-racket-string';
+import { DataControlService } from '../control/data-control.service';
+import { Firebase } from '../utility/firebase';
+import { Constant } from '../utility/constant';
+
+
+@Injectable()
+export class StorageManagementStore {
+
+    constructor(private fetch: DataControlService) {
+        this.updateState()
+    }
+
+    @observable
+    racketStrings: RacketStringModel[] = []
+
+    @action
+    async updateState() {
+        await this.fetch.getList<RacketStringModel>(Firebase.storage + '/' + Constant.shopId).subscribe((items) => {
+            items.sort((a , b) => (a.brand > b.brand) ? 1 : -1)
+            this.racketStrings = items
+        })
+    }
+    
+    
+    
+
+
+
+}
