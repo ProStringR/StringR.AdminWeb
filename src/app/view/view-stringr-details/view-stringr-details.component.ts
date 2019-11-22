268e96eea96e89484ef0005e4e28e848b7f5b1ba
Steen Hansen <steen_home@hotmail.com>
2019-11-22 21:30:36 +0100

    tilføjet kode til stringr detals så den kan tilføjes og fjernes fra listerne.

--- src/app/view/view-stringr-details/view-stringr-details.component.ts
+++ src/app/view/view-stringr-details/view-stringr-details.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { StringrModel } from 'src/app/model/model-stringr';
 
 @Component({
   selector: 'app-view-stringr-details',
   templateUrl: './view-stringr-details.component.html',
   styleUrls: ['./view-stringr-details.component.scss']
 })
-export class ViewStringrDetailsComponent implements OnInit {
+export class ViewStringrDetailsComponent {
+
+  @Input() index: number;
+  @Input() element: StringrModel;
+  @Output() onSuggest: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
-
-  ngOnInit() {
+  
+  pickStringr() {
+    this.onSuggest.emit([this.index]);
   }
 
 }
