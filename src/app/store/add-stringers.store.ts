727d9b759d7308c94dddc767fb196b9d503e5f5d
Steen Hansen <steen_home@hotmail.com>
2019-11-22 20:16:06 +0100

    lavet første del af storen af en stringr

--- src/app/store/add-stringers.store.ts
+++ src/app/store/add-stringers.store.ts
@@ -1,9 +1,8 @@
 import { Injectable } from "@angular/core";
 import { observable, action } from 'mobx-angular';
-import { RacketStringModel } from '../model/model-racket-string';
 import { DataControlService } from '../control/data-control.service';
 import { Firebase } from '../utility/firebase';
-import { Constant } from '../utility/constant';
+import { StringrModel } from '../model/model-stringr';
 
 
 @Injectable(
@@ -12,20 +11,20 @@ import { Constant } from '../utility/constant';
 export class AddStringersStore {
 
     constructor(private fetch: DataControlService) {
-        // this.updateState()
+        this.updateState()
     }
 
-    /*
     @observable
-    racketStrings: RacketStringModel[] = []
+    allStringrs: StringrModel[] = [];
+
+    @observable
+    yourStringrs: StringrModel[] = [];
 
     @action
     async updateState() {
-        await this.fetch.getList<RacketStringModel>(Firebase.storage + '/' + Constant.shopId).subscribe((items) => {
-            items.sort((a , b) => (a.brand > b.brand) ? 1 : -1)
-            this.racketStrings = items
-        })
+        await this.fetch.getList<StringrModel>(Firebase.stringer).subscribe((items) => {
+            this.allStringrs = items;
+        });
     }
-    */
-
+    
 }
