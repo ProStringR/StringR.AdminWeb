4cbfabe2cfdd21bb94b139ce1cb106c3123f28a8
Steen Hansen <steen_home@hotmail.com>
2019-11-26 13:29:09 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ViewStorageManagementModule } from './view/view-storage-management/view-storage-management.module';
 import { DatePipe } from '@angular/common';
 import { DataControlService } from './control/data-control.service';
+
+// Mobx
 import { StorageManagementStore } from './store/storage-management.store';
 import { AddStringersStore } from './store/add-stringers.store';
 
