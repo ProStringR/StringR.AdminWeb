331aa57438a572355085b87f39379da3b1693623
Steen Hansen <steen_home@hotmail.com>
2019-11-29 23:20:32 +0100

    fjernet json fordi det kun var relevant for firebase.

--- src/app/control/data-control.service.ts
+++ src/app/control/data-control.service.ts
@@ -22,7 +22,7 @@ export class DataControlService {
   }
 
   public getList<T>(url: string): Observable<T[]> {
-    return this.http.get<T>(url + '.json').pipe(
+    return this.http.get<T>(url).pipe(
       map(response => {
         const res: T[] = []
         for (const item of Object.keys(response)) {
