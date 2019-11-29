3d15410212598c20d434dfa3990637ee4836a763
Steen Hansen <steen_home@hotmail.com>
2019-11-29 23:22:00 +0100

    Færdig med første eteappe af order store.

--- src/app/store/orders-overview.store.ts
+++ src/app/store/orders-overview.store.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
-import { observable, action } from 'mobx-angular';
-import { RacketStringModel } from '../model/model-racket-string';
+import { observable, action, computed } from 'mobx-angular';
 import { DataControlService } from '../control/data-control.service';
 import { API } from '../config/api'
+import { OrderModel } from '../model/model-order';
 
 @Injectable(
     { providedIn: 'root' }
@@ -10,8 +10,32 @@ import { API } from '../config/api'
 export class OrdersOverviewStore {
 
     constructor(private fetch: DataControlService) {
-        this.fetch.firstGet(API.order + '/1').subscribe((res) => {
-            console.log(res)
+        this.updateState();
+    }
+
+    @observable
+    allOrdersForShop: Array<OrderModel[]> = Array<OrderModel[]>(4);
+
+    @action
+    async updateState() {
+        let received: OrderModel[] = [];
+        let done: OrderModel[] = [];
+        let delivered: OrderModel[] = [];
+        let completed: OrderModel[] = [];
+
+        this.fetch.getList<OrderModel>(API.orderShopId + '/1').subscribe((res) => {
+            res.forEach((order) => {
+                console.log(order)
+                switch (order.orderStatus) {
+                    case 0: received.push(order); break;
+                    case 1: done.push(order); break;
+                    case 2: delivered.push(order); break;
+                    case 3: completed.push(order); break;
+                    default:
+                        break;
+                }
+            })
+            this.allOrdersForShop = [received, done, delivered, completed];
         })
     }
 
