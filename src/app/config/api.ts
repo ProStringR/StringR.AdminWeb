163fc365d5d9ea7c7f8faf4eaa5788cadb097be5
Steen Hansen <steen_home@hotmail.com>
2019-12-02 16:58:19 +0100

    Lavet alle vores API endpoint i koden.

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,3 +1,57 @@
 export class API {
-    static orderShopId = '/api/v1/order/shop'
+
+    /**
+     * Authentication
+     */
+    static post_auth = '/api/v1/Authentication';
+
+    /**
+     * Boss
+     */
+    static get_boss = '/api​/Boss';
+
+    /**
+     * Customer
+     */
+    static get_customer = '/api/v1/Customer';
+    static get_customer_by_id(customerId): string { return `/api/v1/Customer/${customerId}` };
+    static post_customer = '/api/v1/Customer';
+
+    /**
+     * Order
+     */
+    static get_order = '/api/v1/Order';
+    static get_order_by_orderId(orderId): string { return `/api/v1/Order/${orderId}` };
+    static post_order = '/api/v1/Order';
+    static put_order = '/api/v1/Order';
+    static get_order_by_shopId(shopId): string { return `/api/v1/order/shop/${shopId}` };
+    static get_order_by_shopId_orderStatus(shopId, orderStatus): string { return `/api/v1/Order/shop/${shopId}/${orderStatus}` };
+
+    /**
+     * Racket
+     */
+    static post_racket = '/api​/v1​/Racket';
+
+    /**
+     * RacketString
+     */
+    static get_racketString_by_racketStringId(racketStringId): string { return `/api/v1/RacketString/${racketStringId}` };
+    static get_racketString_by_shopId(shopId): string { return `/api/v1/RacketString/shop/${shopId}` };
+    static put_racketString = '/api/v1/RacketString';
+
+    /**
+     * Shop
+     */
+    static get_stop_by_shopId(shopId): string { return `/api/v1/Shop/${shopId}` };
+    static post_shop = '/api/v1/Shop';
+    static post_shop_teamWithMember = '/api/v1/Shop/postTeamWithMember';
+
+    /**
+     * Stringer
+     */
+    static get_stringer = '​/api​/v1​/Stringer';
+    static post_stringer = '/api/v1/Stringer';
+    static get_stringer_by_stringerId(stringerId): string { return `​/api​/v1​/Stringer​/${stringerId}` };
+    static get_stringer_by_shopId(shopId): string { return `/api/v1/Stringer/shop/${shopId}` };
+
 }
