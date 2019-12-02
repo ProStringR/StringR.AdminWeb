fb6e30af6f5cc48859a447a6fafa8d5f09aa4461
Steen Hansen <steen_home@hotmail.com>
2019-12-02 19:20:46 +0100

    Bygget ny model ud fra ny database.

--- src/app/model/model-racket-string.ts
+++ src/app/model/model-racket-string.ts
@@ -1,23 +1,12 @@
-import { RacketTypeEnum } from '../enum/enum-racket-type';
-import { StringBrandEnum } from '../enum/enum-string-brand';
-import { StringTypeEnum } from '../enum/enum-string-type';
-import { StringColorEnum } from '../enum/enum-string-color';
-
 export interface RacketStringModel {
-  stringId: string;
-  brand: StringBrandEnum;
-  modelName: string;
-  stringType: StringTypeEnum;
-  length: number;
-  pricePerRacket: number;
-  thickness: number;
-  color: StringColorEnum;
-  stringPurpose: RacketTypeEnum;
-  purchaseHistory: [PurchaseHistory];
-}
-
-export interface PurchaseHistory {
-  date: number;
-  length: number;
-  price: number;
+  id: number
+  shopId: number
+  lengthInStock: number
+  price: number
+  stringModel: string
+  stringType: string
+  stringBrand: string
+  stringThickness: number
+  stringPurpose: string
+  stringColor: string
 }
