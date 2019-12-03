d08b055ea64a2f1967525ac02b4ec436430593bc
Steen Hansen <steen_home@hotmail.com>
2019-12-03 14:44:55 +0100

    Fjernet ubrugt kode og gjordt klar til login. Backenden er måske ikke klar ?

--- src/app/view/view-login/view-login.component.ts
+++ src/app/view/view-login/view-login.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-view-login',
   templateUrl: './view-login.component.html',
   styleUrls: ['./view-login.component.scss']
 })
-export class ViewLoginComponent implements OnInit {
 
-  constructor(private router: Router) {}
+export class ViewLoginComponent {
 
-  ngOnInit() {
-    // TODO - Skal fjernes når authentication service er færdigt.
-    //this.router.navigate(['mainPage'])
-  }
+  // TODO Router skal fjernes helt når authguard kører.
+  constructor(private authService: AuthService, private router: Router) { }
 
-  login() {
-    this.router.navigate(['/mainPage'])
+  login(form: NgForm) {
+    this.authService.login(form.value.username, form.value.password);
+    //this.router.navigate(['/mainPage']);
   }
 
 }
