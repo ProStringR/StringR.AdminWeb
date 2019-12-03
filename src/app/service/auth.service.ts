13493089a43db54f5bd4d3ee0cc44604f796f8a8
Steen Hansen <steen_home@hotmail.com>
2019-12-03 16:24:46 +0100

    Update auth.service.ts

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,7 +3,6 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { DataControlService } from '../control/data-control.service';
 import { API } from './../config/api';
 import { User } from './../model/model-user';
-import { HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Injectable(
@@ -20,9 +19,6 @@ export class AuthService {
 
   public isAuthenticated(): boolean {
     const token = localStorage.getItem('token');
-
-    console.log(this.jwtHelper.isTokenExpired(token))
-
     return !this.jwtHelper.isTokenExpired(token);
   }
 
