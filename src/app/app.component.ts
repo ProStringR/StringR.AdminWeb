070989b9793dd34ad1620a89d6f1112b29350406
Steen Hansen <steen_home@hotmail.com>
2019-12-06 11:58:27 +0100

    Rykket alt over til en routes config, for at forhindre stavefejl.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Routes } from './config/config';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,6 @@ export class AppComponent {
   title = 'AdminWeb';
 
   constructor(private router: Router) {
-    this.router.navigate(['/mainPage']);
+    this.router.navigate([Routes.mainPage]);
   }
 }
