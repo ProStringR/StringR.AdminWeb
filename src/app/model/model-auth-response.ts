3101594b2f0d4a18fac2467d09fc91ff26776b23
Steen Hansen <steen_home@hotmail.com>
2019-12-06 13:41:38 +0100

    Givet Auth user en constructor.

--- src/app/model/model-auth-response.ts
+++ src/app/model/model-auth-response.ts
@@ -3,3 +3,16 @@ export interface ModelAuthResponse {
   id: number;
   responseMessage: string;
 }
+
+export class AuthUser implements ModelAuthResponse {
+  public token: string;
+  public id: number;
+  public responseMessage: string;
+
+  constructor(token: string, id: number, responseMessage: string) {
+    this.token = token;
+    this.id = id;
+    this.responseMessage = responseMessage;
+  }
+
+}
