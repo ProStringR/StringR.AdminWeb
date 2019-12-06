dfb2ca140f1a504c7c7ec6e167559fed153908dc
Steen Hansen <steen_home@hotmail.com>
2019-12-06 13:42:41 +0100

    Update storage-management.store.ts

--- src/app/store/storage-management.store.ts
+++ src/app/store/storage-management.store.ts
@@ -1,24 +1,27 @@
 import { Injectable } from "@angular/core";
-import { observable, action, computed } from 'mobx-angular';
+import { observable, action } from 'mobx-angular';
 import { RacketStringModel } from '../model/model-racket-string';
 import { DataControlService } from '../control/data-control.service';
-import { API } from '../config/api';
+import { API } from '../config/config';
+import { AuthService } from '../service/auth.service';
 
 @Injectable(
     { providedIn: 'root' }
 )
+
 export class StorageManagementStore {
 
-    constructor(private fetch: DataControlService) {
-        this.updateState()
-    }
+    constructor(
+        private fetch: DataControlService,
+        private auth: AuthService
+    ) { }
 
     @observable
     racketStrings: RacketStringModel[] = []
 
     @action
     async updateState() {
-        await this.fetch.getList<RacketStringModel>(API.get_racketString_by_shopId(1)).subscribe((items) => {
+        await this.fetch.getList<RacketStringModel>(API.get_racketString_by_shopId(this.auth.getUser().id)).subscribe((items) => {
             items.sort((a, b) => (a.stringBrand > b.stringBrand) ? 1 : -1)
             this.racketStrings = items
         })
