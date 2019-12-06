4377eebc0724fc1368953c2c50ccc4b8e00d303e
Steen Hansen <steen_home@hotmail.com>
2019-12-06 13:42:23 +0100

    Update add-stringers.store.ts

--- src/app/store/add-stringers.store.ts
+++ src/app/store/add-stringers.store.ts
@@ -2,7 +2,8 @@ import { Injectable } from "@angular/core";
 import { observable, action, computed } from 'mobx-angular';
 import { DataControlService } from '../control/data-control.service';
 import { StringrModel } from '../model/model-stringr';
-import { API } from '../config/api';
+import { API } from '../config/config';
+import { AuthService } from '../service/auth.service';
 
 @Injectable(
     { providedIn: 'root' }
@@ -10,9 +11,10 @@ import { API } from '../config/api';
 
 export class AddStringersStore {
 
-    constructor(private fetch: DataControlService) {
-        this.updateState()
-    }
+    constructor(
+        private fetch: DataControlService,
+        private auth: AuthService
+    ) { }
 
     @observable
     potentialStringrs: StringrModel[] = [];
@@ -27,23 +29,24 @@ export class AddStringersStore {
     filterYours: string = "";
 
     @action
-    async updateState() {
-        await this.fetch.getList<StringrModel>(API.get_stringer_by_shopId(1)).subscribe((stringrs) => {
-            this.potentialStringrs = stringrs;
+    public async updateState() {
+        await this.fetch.getList<StringrModel>(API.get_stringer_by_shopId(this.auth.getUser().id)).subscribe((stringrs) => {
+            console.log(stringrs);
+            this.yourStringrs = stringrs;
         });
     }
 
     @computed
-    get allStringrsFiltered(): StringrModel[] {
+    public get allStringrsFiltered(): StringrModel[] {
         return this.potentialStringrs.filter((stringr) => JSON.stringify(stringr).toLowerCase().includes(this.filterPotential.toLowerCase()));
     }
 
     @computed
-    get yourStringrsFiltered(): StringrModel[] {
+    public get yourStringrsFiltered(): StringrModel[] {
         return this.yourStringrs.filter((stringr) => JSON.stringify(stringr).toLowerCase().includes(this.filterYours.toLowerCase()));
     }
 
-    async updateBackend() {
+    public async updateBackend() {
         // TODO Save yourStringrs to backend.
     }
 
